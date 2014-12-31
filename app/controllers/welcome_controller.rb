bb98a45d0dc770869564adc6825a25d196d920bd
Akira Yumiyama <yumiyama.akira@gmail.com>
2014-12-31 23:57:40 +0900

    welcome page

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,14 @@
+class WelcomeController < ApplicationController
+  def index
+    # render welcome page
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :timers
+  resources :rooms do
+    resources :timers
+  end
 
-  resources :rooms
+  root 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
